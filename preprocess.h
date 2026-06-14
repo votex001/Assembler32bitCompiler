c232903d929f0f09a471cb545676013fd1dcbb9a
votex001 <jeneea45@gmail.com>
2026-06-14 21:52:34 +0300

    Update preprocess.h

--- preprocess.h
+++ preprocess.h
@@ -1,7 +1,10 @@
 #ifndef PREPROCESS_H
 #define PREPROCESS_H
+#include <stdio.h>
+#include "global.h"
 
 
-
+bool expandMacros(cur_line line);
+bool preprocessFile(FILE *file,char *fileName);
 
 #endif
