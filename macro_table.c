4b4c0c4438699e35fb9b2c2bc862550ba25d3f26
votex001 <jeneea45@gmail.com>
2026-07-08 23:29:08 +0300

    added func to save malloc strs

---
+++ macro_table.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <string.h>
+#include "global.h"
+#include "macro_table.h"
+#include "utils.h"
+
+
+
+
+/*global table*/
+macro_table macro_head;
+
+
+
+
+
+
+
+
+
+void appendMacroLine(char *name,char *content){
+
+    macro *macro_ptr,*current;
+    char *temp_content;
+
+    
+
+    
+    /*we dont have macros until now*/
+    if(macro_head == NULL){
+        macro_ptr = (macro *) mallocWithCheck(sizeof(*macro));
+        macro_ptr->name = (char *) mallocWithCheck(strlen(name)+1);
+        macro_ptr->content = (char *) mallocWithCheck(strlen(content)+1);
+        macro_ptr->next = NULL;
+        strcpy(macro_ptr->name,name);
+        strcpy(macro_ptr->content,content);
+        macro_head = macro_ptr;
+        return;
+    }
+
+    current = macro_head;
+    /*finding our macro*/
+    while(current->next != NULL && strcmp(current->name, name) != 0)current=current->next;
+
+    /*if we find name adding content else we adding new macro in the end of arr*/
+    if(strcmp(current->name,name)==0){
+        current->content = reallocWithCheck(current->content,
+                                            strlen(current->content) + 
+                                            strlen(content) + 1);
+        strcat(current->content,content);                                    
+    }else{
+        macro_ptr = (macro *) mallocWithCheck(sizeof(*macro));
+        macro_ptr->name = (char *) mallocWithCheck(strlen(name)+1);
+        macro_ptr->content = (char *) mallocWithCheck(strlen(content)+1);
+        macro_ptr->next = NULL;
+        strcpy(macro_ptr->name,name);
+        strcpy(macro_ptr->content,content);
+        current->next = macro_ptr;
+    }
+}
--- macro_table.h
+++ macro_table.h
@@ -1,15 +1,17 @@
 #ifndef MACRO_TABLE_H
 #define MACRO_TABLE_H
 
-typedef struct macro_table
+typedef struct macro
 {
     char *name;
 
     char *content;
 
-    struct macro_table *next;
-} macro_table;
+    struct macro *next;
+} macro;
 
+typedef macro *macro_table;
 
+void appendMacroLine(char *name,char *content);
 
 #endif
